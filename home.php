e0d0a51f43888d228a763e9db0bd3eedcf76a20c
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-05-20 15:01:50 +0800

    initial commit

---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&family=Noto+Serif+TC:wght@200;300;400;500;600;700;900&family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">
+
+    <title>租隊友</title>
+</head>
+<body>
+    <section id="banner">
+        <div class="banner-wrapper">
+            <img src="https://picsum.photos/300/200" alt="placeholder">
+            <h1 class="banner-title">&nbsp租隊友</h1>
+        </div>
+    </section>
+
+    <section id="landing-zone">
+        <div class="landing-wrapper">
+            <div class="lw-left">
+                <h1>租屋進行式</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni praesentium ullam a. Nesciunt, at necessitatibus adipisci molestias impedit quas debitis culpa? Adipisci sunt facilis magnam veritatis vero maxime magni harum.</p>
+            </div>
+            <div class="lw-right">
+                <img src="https://picsum.photos/600/800" alt="">
+            </div>
+        </div>
+    </section>
+</body>
+</html>
