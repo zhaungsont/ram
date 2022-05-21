72dec6e5c73b9a95e486f857ae800115c65175b1
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-05-21 14:52:34 +0800

    build basic login system

--- login.php
+++ login.php
@@ -26,14 +26,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&family=Noto+Serif+TC:wght@200;300;400;500;600;700;900&family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
     <h1>Log in</h1>
-    <form action="auth.php" method="POST">
+    <form action="loginauth.php" method="POST">
         帳號 <input type="text" name="account">
         密碼 <input type="password" name="password">
         <button type="submit" class="btn btn-dark btn-lg">登入</button>
+        <a href="register.php" class="btn btn-light btn-lg" role="button">註冊</a>
     </form>
+
 </body>
 </html>
--- loginauth.php
+++ loginauth.php
@@ -18,21 +18,32 @@
     } else {
         echo "MySQL ram 資料庫連接成功<br>";
         // $sql = "SELECT * FROM user;";
-        $sql = "SELECT *  FROM `user` WHERE `account` = \'zhsont\' AND `pw` = \'1234\';";
+        $sql = "SELECT *  FROM user WHERE account = '$inputAccount' AND password = '$inputPassword';";
         $result = mysqli_query($link, $sql);
-        var_dump($result);
         $resultCheck = mysqli_num_rows($result);
 
         if ($resultCheck > 0){
+            // 資料庫內有這個帳號
             while ($row = mysqli_fetch_assoc($result)){
                 echo $row['uid']."<br>";
                 echo $row['name']."<br>";
                 echo $row['account']."<br>";
                 echo $row['pw']."<br>";
             }
-        } else {
+            echo "<br>";
 
-            echo "aa<br>";
+        } else {
+            // 資料庫內沒有查到這個帳號
+            // 所以將使用者重新導向回登入頁面
+            // 有兩種方式：
+            // 第一種：在網頁上寫
+            echo "<h1>登入失敗！ 5 秒後將你跳轉回登入畫面</h1>";
+
+            // 第二種：用跳出式提醒
+            $message = "帳號或密碼錯誤！";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+            header( "refresh:0;url=login.php" );
+
         }
     }
 
