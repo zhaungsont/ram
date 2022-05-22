6203cf85f0ef0b29735b16fbb9694ce40aee9bb5
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-05-22 20:14:05 +0800

    build createlisting infastructure

---
+++ createlisting.php
@@ -0,0 +1,42 @@
+<?php
+
+    session_start();
+
+    $link = mysqli_connect(
+        'localhost', // mysql 主機名稱
+        'root', // 使用者名稱
+        '', // 密碼
+        'ram' // 預設使用的資料庫名稱
+    );
+
+    if (!$link) {
+        echo "MySQL 連線錯誤<br>";
+        exit();
+    } 
+    if (!isset($_SESSION['uid'])){
+        header("Location: login.php");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- 共用CSS -->
+    <link rel="stylesheet" href="styles/common.css">
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&family=Noto+Serif+TC:wght@200;300;400;500;600;700;900&family=ZCOOL+QingKe+HuangYou&display=swap" rel="stylesheet">
+    <title>刊登在租隊友上</title>
+</head>
+<body>
+<?php require('require/header.php'); ?>
+刊登屋件
+
+</body>
+</html>
