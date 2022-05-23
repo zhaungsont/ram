d41e29ef91fc3dc6d350271b5467c9e112abc35f
Michael Chuang <81564341+zhaungsont@users.noreply.github.com>
2022-05-23 18:50:06 +0800

    add listing creation logic after submitting

--- creationauth.php
+++ creationauth.php
@@ -1,6 +1,13 @@
 <?php
 
-echo "yo";
+session_start();
+
+if (!isset($_SESSION['uid'])){
+    header('Location: login.php');
+} elseif (!isset($_POST['hname'])){
+    header('Location: createlisting.php');
+}
+
 
 
 ?>
